ffec053371a17f006c4376ee4f93b59474f8cfa0
Aymn ibrahim <Aymnibrahim730@gmail.com>
2024-01-27 01:14:24 +0300

    Complete GenerateDataAccessLayer and BusinessLayer classes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,12 @@
             this.btnFile = new System.Windows.Forms.Button();
             this.cbDataBases = new System.Windows.Forms.ComboBox();
             this.cbTables = new System.Windows.Forms.ComboBox();
+            this.listViewColumns = new System.Windows.Forms.ListView();
+            this.columnDataType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnAllowNull = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnIsPrimary = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnGenerateDataAccessLayer = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtDataBaseName
@@ -59,10 +65,10 @@
             // 
             // Text1
             // 
-            this.Text1.Location = new System.Drawing.Point(12, 292);
+            this.Text1.Location = new System.Drawing.Point(12, 456);
             this.Text1.Multiline = true;
             this.Text1.Name = "Text1";
-            this.Text1.Size = new System.Drawing.Size(776, 335);
+            this.Text1.Size = new System.Drawing.Size(1065, 443);
             this.Text1.TabIndex = 2;
             // 
             // txtConnectionString
@@ -76,7 +82,7 @@
             // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(348, 74);
+            this.btnLoad.Location = new System.Drawing.Point(301, 30);
             this.btnLoad.Name = "btnLoad";
             this.btnLoad.Size = new System.Drawing.Size(129, 40);
             this.btnLoad.TabIndex = 4;
@@ -95,18 +101,19 @@
             // 
             // btnFile
             // 
-            this.btnFile.Location = new System.Drawing.Point(348, 194);
+            this.btnFile.Location = new System.Drawing.Point(301, 86);
             this.btnFile.Name = "btnFile";
             this.btnFile.Size = new System.Drawing.Size(129, 40);
             this.btnFile.TabIndex = 6;
             this.btnFile.Text = "button1";
             this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
             // 
             // cbDataBases
             // 
             this.cbDataBases.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbDataBases.FormattingEnabled = true;
-            this.cbDataBases.Location = new System.Drawing.Point(599, 74);
+            this.cbDataBases.Location = new System.Drawing.Point(599, 23);
             this.cbDataBases.Name = "cbDataBases";
             this.cbDataBases.Size = new System.Drawing.Size(189, 24);
             this.cbDataBases.TabIndex = 7;
@@ -116,17 +123,70 @@
             // 
             this.cbTables.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbTables.FormattingEnabled = true;
-            this.cbTables.Location = new System.Drawing.Point(599, 161);
+            this.cbTables.Location = new System.Drawing.Point(599, 63);
             this.cbTables.Name = "cbTables";
             this.cbTables.Size = new System.Drawing.Size(189, 24);
             this.cbTables.TabIndex = 8;
             this.cbTables.SelectedIndexChanged += new System.EventHandler(this.cbTables_SelectedIndexChanged);
             // 
+            // listViewColumns
+            // 
+            this.listViewColumns.AllowColumnReorder = true;
+            this.listViewColumns.AutoArrange = false;
+            this.listViewColumns.CheckBoxes = true;
+            this.listViewColumns.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnDataType,
+            this.columnName,
+            this.columnAllowNull,
+            this.columnIsPrimary});
+            this.listViewColumns.GridLines = true;
+            this.listViewColumns.HideSelection = false;
+            this.listViewColumns.HoverSelection = true;
+            this.listViewColumns.Location = new System.Drawing.Point(325, 161);
+            this.listViewColumns.Name = "listViewColumns";
+            this.listViewColumns.Size = new System.Drawing.Size(373, 271);
+            this.listViewColumns.TabIndex = 9;
+            this.listViewColumns.UseCompatibleStateImageBehavior = false;
+            this.listViewColumns.View = System.Windows.Forms.View.Details;
+            this.listViewColumns.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.listViewColumns_ItemCheck);
+            // 
+            // columnDataType
+            // 
+            this.columnDataType.Text = "Data Type";
+            this.columnDataType.Width = 80;
+            // 
+            // columnName
+            // 
+            this.columnName.Text = "Column Name";
+            this.columnName.Width = 115;
+            // 
+            // columnAllowNull
+            // 
+            this.columnAllowNull.Text = "Allow Null";
+            this.columnAllowNull.Width = 80;
+            // 
+            // columnIsPrimary
+            // 
+            this.columnIsPrimary.Text = "Is Primary Key";
+            this.columnIsPrimary.Width = 100;
+            // 
+            // btnGenerateDataAccessLayer
+            // 
+            this.btnGenerateDataAccessLayer.Location = new System.Drawing.Point(455, 86);
+            this.btnGenerateDataAccessLayer.Name = "btnGenerateDataAccessLayer";
+            this.btnGenerateDataAccessLayer.Size = new System.Drawing.Size(129, 40);
+            this.btnGenerateDataAccessLayer.TabIndex = 10;
+            this.btnGenerateDataAccessLayer.Text = "Generate Data Access Layer";
+            this.btnGenerateDataAccessLayer.UseVisualStyleBackColor = true;
+            this.btnGenerateDataAccessLayer.Click += new System.EventHandler(this.btnGenerateDataAccessLayer_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 639);
+            this.ClientSize = new System.Drawing.Size(1089, 911);
+            this.Controls.Add(this.btnGenerateDataAccessLayer);
+            this.Controls.Add(this.listViewColumns);
             this.Controls.Add(this.cbTables);
             this.Controls.Add(this.cbDataBases);
             this.Controls.Add(this.btnFile);
@@ -155,6 +215,12 @@
         private System.Windows.Forms.Button btnFile;
         private System.Windows.Forms.ComboBox cbDataBases;
         private System.Windows.Forms.ComboBox cbTables;
+        private System.Windows.Forms.ListView listViewColumns;
+        private System.Windows.Forms.ColumnHeader columnDataType;
+        private System.Windows.Forms.ColumnHeader columnName;
+        private System.Windows.Forms.ColumnHeader columnAllowNull;
+        private System.Windows.Forms.ColumnHeader columnIsPrimary;
+        private System.Windows.Forms.Button btnGenerateDataAccessLayer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,34 +16,82 @@ namespace Code_Generator_Data_Access_and_Business_Layer_
         public Form1()
         {
             InitializeComponent();
-  
+
         }
 
 
         static GenerateDataAccessLayer.TableColumnInfo PrimaryColumn = new GenerateDataAccessLayer.TableColumnInfo();
 
         static List<GenerateDataAccessLayer.TableColumnInfo> ListTableContact = new List<GenerateDataAccessLayer.TableColumnInfo>();
+        static List<GenerateDataAccessLayer.TableColumnInfo> ListSelectedRows = new List<GenerateDataAccessLayer.TableColumnInfo>();
 
         static string ConnectionString = "Server = .; Database = ContactsDB; User ID = sa ; Password = sa123456";
 
-     
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
+        private void _setOneItem(GenerateDataAccessLayer.TableColumnInfo Column)
+        {
+
+      
+            ListViewItem Item = new ListViewItem(Column.DataType);
+
+            Item.SubItems.Add(Column.ColumnName);
+            Item.SubItems.Add(Column.AllowNull.ToString());
+            Item.SubItems.Add(Column.IsPrimaryKey.ToString());
+
+            listViewColumns.Items.Add(Item);
+        }
+
+        private void _RemoveItemsFromListView()
+        {
+            while (this.listViewColumns.Items.Count > 0)
+            {
+                this.listViewColumns.Items[0].Remove();
+            }
+        }
+
+        private void setTableColumnsInListView()
+        {
+            _RemoveItemsFromListView();
+
+            foreach (GenerateDataAccessLayer.TableColumnInfo i in ListTableContact)
+            {
+                _setOneItem(i);
+            }
+
+            
+        }
+
+        private void FullListWithCheckedItem(ListView.CheckedListViewItemCollection checkedItems)
+        {
+            string firstSubItemValue = "";
+            GenerateDataAccessLayer.TableColumnInfo CheckedItem = new GenerateDataAccessLayer.TableColumnInfo();
+
+            ListSelectedRows.Clear();
+            foreach (ListViewItem item in checkedItems)
+            {
+                
+                firstSubItemValue = item.SubItems[1].Text;
+                CheckedItem = GenerateDataAccessLayer.FindColumn(ListTableContact, firstSubItemValue);
+
+                if (CheckedItem.IsPrimaryKey)
+                    continue;
+
+                ListSelectedRows.Add(CheckedItem);
+
+                
+            }
+        }
+
         private void btnLoad_Click(object sender, EventArgs e)
         {
             DataTable TableInfo = GenerateDataAccessLayer.GetTableInformations(txtConnectionString.Text, txtDataBaseName.Text, txtTableName.Text);
             GenerateDataAccessLayer.LoadListWithTableInfo(TableInfo, ref ListTableContact, ref PrimaryColumn);
-
-            //foreach(GenerateDataAccessLayer.TableColumnInfo columnInfo in ListTableContact)
-            //{
-            //    Text1.Text += $"Column Name: {columnInfo.ColumnName}, Data Type: {columnInfo.DataType}, Allows Null: {columnInfo.AllowNull}, Is Primary Key: {columnInfo.IsPrimaryKey}\n";
-            //}
-
-            //Text1.Text = GenerateDataAccessLayer.GetContactInfoByID(ListTableContact,PrimaryColumn,txtTableName.Text,txtClassName.Text);
 
             List<string> Hello = clsDataBaseConnection.DisplayDatabases(ConnectionString);
 
@@ -72,6 +120,32 @@ namespace Code_Generator_Data_Access_and_Business_Layer_
             ListTableContact.Clear();
             DataTable TableInfo = GenerateDataAccessLayer.GetTableInformations(txtConnectionString.Text, cbDataBases.SelectedItem.ToString(), cbTables.SelectedItem.ToString());
             GenerateDataAccessLayer.LoadListWithTableInfo(TableInfo, ref ListTableContact, ref PrimaryColumn);
+
+            setTableColumnsInListView();
+        }
+
+        private void listViewColumns_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnGenerateDataAccessLayer_Click(object sender, EventArgs e)
+        {
+            //Load Checked subItem in List
+            ListView.CheckedListViewItemCollection checkedItems = listViewColumns.CheckedItems;
+            FullListWithCheckedItem(checkedItems);
+
+             Text1.Text = GenerateDataAccessLayer.Generate_DataAccessLayer(ListTableContact, ListSelectedRows, PrimaryColumn, cbTables.SelectedItem.ToString(), txtClassName.Text);
+
+            //Text1.Text = GenerateBusinessLayer.BusinessLayer_GetAllIsContactExist(ListSelectedRows, txtClassName.Text);
+
+            //Text1.Text = GenerateBusinessLayer.Generate_BusinessLayer(ListTableContact,ListSelectedRows,PrimaryColumn,cbTables.Text.ToString(),txtClassName.Text);
         }
     }
 }
